7a25f55de93822a020d96bb00855cb6f8d2d52ae
Elvin Aliyev <elvinaa@code.edu.az>
2020-11-18 11:38:22 +0400

    empty introduction

---
+++ index.js
@@ -0,0 +1,10 @@
+const code ='AZN'
+if (code === 'AZN') {
+    console.log('manat');
+} else if (code === 'USD') {
+    console.log('dollar');
+} else if (code === 'EUR') {
+    console.log('euro');
+} else {
+    console.log('error');
+}
